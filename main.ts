09cc42ff0cd797a4169f9a8fde7992ae05e8e0d4
Shyg0l <167764221+Shyg0l@users.noreply.github.com>
2024-04-22 12:53:07 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    men1()
+})
 function men1 () {
     while (!(input.buttonIsPressed(Button.A))) {
-    	
+        summe = 0
+        for (let index = 0; index < 10; index++) {
+            summe += input.soundLevel()
+        }
+        basic.showNumber(summe / 10)
+        basic.pause(500)
     }
 }
+let summe = 0
 let menue = 1
 basic.showNumber(menue)
 basic.forever(function () {
