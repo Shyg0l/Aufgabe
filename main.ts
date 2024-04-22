634052b324d64daa6a355e0db9f0b8dec6702102
Shyg0l <167764221+Shyg0l@users.noreply.github.com>
2024-04-22 12:44:38 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let menue = 1
+basic.showNumber(menue)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        menue += 1
+        if (menue > 5) {
+            menue = 1
+        }
+        basic.showNumber(menue)
+    }
 })
